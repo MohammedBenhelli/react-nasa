91cd01f1fe9283b06c869d3756f8586e75a825aa
mohammed <mohammed.benhelli@epitech.eu>
2020-10-05 20:55:00 +0200

    first commit

---
+++ precache-manifest.46b607f7c628a9544bc17782db6200b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a848dd8b31f115f73402a9706e949906",
+    "url": "./index.html"
+  },
+  {
+    "revision": "696a43031e9c579f0926",
+    "url": "./static/js/2.c10999f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.c10999f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae64a3539fbcd961a85",
+    "url": "./static/js/main.80ab637a.chunk.js"
+  },
+  {
+    "revision": "fb32fc65a07c0b5e9467",
+    "url": "./static/js/runtime-main.36c155fd.js"
+  }
+]);
